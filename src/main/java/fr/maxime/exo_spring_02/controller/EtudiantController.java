a2c6e50eef4cb476a9a4450e8cb26725b6c1afa8
MAGICTNT <maxjavaux@gmail.com>
2024-09-11 14:08:04 +0200

    update form et error page

--- src/main/java/fr/maxime/exo_spring_02/controller/EtudiantController.java
+++ src/main/java/fr/maxime/exo_spring_02/controller/EtudiantController.java
@@ -2,8 +2,10 @@ package fr.maxime.exo_spring_02.controller;
 
 import fr.maxime.exo_spring_02.model.Etudiant;
 import fr.maxime.exo_spring_02.service.EtudiantService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,14 +30,15 @@ public class EtudiantController {
     }
 
     @RequestMapping("/inscription")
-    public String inscription() {
+    public String inscription(Model model) {
+        model.addAttribute("etudiant", new Etudiant());
         return "Inscription";
     }
 
     @RequestMapping("/list")
     public String listEtudiant(Model model) {
         model.addAttribute("etudiants", etudiantService.getAllEtudiants());
-        model.addAttribute("message","Liste de tout les étudiant");
+        model.addAttribute("message", "Liste de tout les étudiant");
         return "ListEtudiants";
     }
 
@@ -50,23 +53,38 @@ public class EtudiantController {
         return "RechercheEtudiant";
     }
 
-    @RequestMapping("/inscription/newEtudiant")
-    public String inscription(@RequestParam(name = "nom", required = false) String nom,
-                              @RequestParam(name = "prenom", required = false) String prenom,
-                              @RequestParam(name = "age", required = false) int age,
-                              @RequestParam(name = "mail", required = false) String mail,
-                              Model model) {
-        Etudiant newEtudiant = Etudiant.builder()
-                .id(UUID.randomUUID())
-                .nom(nom)
-                .prenom(prenom)
-                .age(age)
-                .email(mail)
-                .build();
-        etudiantService.addEtudiant(newEtudiant);
-        model.addAttribute("etudiants", etudiantService.getAllEtudiants());
-        return "ListEtudiants";
+    @PostMapping("/inscription")
+    public String inscriptionEtudiant(Model model,
+                                      @Valid @ModelAttribute("etudiant") Etudiant etudiant,
+                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "Inscription";
+        }else {
+            etudiantService.addEtudiant(etudiant);
+
+            model.addAttribute("etudiants", etudiantService.getAllEtudiants());
+
+            return "ListEtudiants";
+        }
+
     }
+//    @RequestMapping("/inscription/newEtudiant")
+//    public String inscription(@RequestParam(name = "nom", required = false) String nom,
+//                              @RequestParam(name = "prenom", required = false) String prenom,
+//                              @RequestParam(name = "age", required = false) int age,
+//                              @RequestParam(name = "mail", required = false) String mail,
+//                              Model model) {
+//        Etudiant newEtudiant = Etudiant.builder()
+//                .id(UUID.randomUUID())
+//                .nom(nom)
+//                .prenom(prenom)
+//                .age(age)
+//                .email(mail)
+//                .build();
+//        etudiantService.addEtudiant(newEtudiant);
+//        model.addAttribute("etudiants", etudiantService.getAllEtudiants());
+//        return "ListEtudiants";
+//    }
 
     @RequestMapping("/detail/{idContact}")
     public String contactPage(Model model, @PathVariable("idContact") UUID id) {
@@ -86,7 +104,7 @@ public class EtudiantController {
             etudiantService.deleteEtudiant(etudiant);
             List<Etudiant> listEtudiant = etudiantService.getAllEtudiants();
             model.addAttribute("etudiants", listEtudiant);
-            model.addAttribute("message","suppression réussi");
+            model.addAttribute("message", "suppression réussi");
             return "ListEtudiants";
         }
         return "ListEtudiants";
@@ -114,27 +132,28 @@ public class EtudiantController {
 
 
     @RequestMapping("/recherche")
-    public String contactForm(Model model){
+    public String contactForm(Model model) {
         model.addAttribute("etudiant", new Etudiant());
         return "RechercheEtudiant";
     }
+
     @PostMapping("/recherche/name")
-    public String submitContact(@ModelAttribute("name") String name){
+    public String submitContact(@ModelAttribute("name") String name) {
         Etudiant etudiantSearch = etudiantService.getEtudiantByName(name);
         System.out.println(etudiantSearch);
-        if(etudiantSearch != null){
+        if (etudiantSearch != null) {
             return "redirect:/detail/name" + etudiantSearch.getNom();
-        }
-        else {
+        } else {
             return "redirect:/recherche";
         }
 //        if(contact != null){
 //
 //        }
     }
+
     @PostMapping("/update")
     public String updateEtudiant(Model model, @ModelAttribute("etudiant") Etudiant etudiant) {
-        Etudiant updateEtudiant = etudiantService.updateEtudiant(etudiant.getId() , etudiant);
+        Etudiant updateEtudiant = etudiantService.updateEtudiant(etudiant.getId(), etudiant);
         if (updateEtudiant != null) {
             model.addAttribute("etudiants", etudiantService.getAllEtudiants());
             return "ListEtudiants";
--- src/main/java/fr/maxime/exo_spring_02/model/Etudiant.java
+++ src/main/java/fr/maxime/exo_spring_02/model/Etudiant.java
@@ -1,5 +1,6 @@
 package fr.maxime.exo_spring_02.model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,16 @@ import java.util.UUID;
 @Builder
 public class Etudiant {
     private UUID id;
+    @NotBlank
+    @NotNull(message = "Ce champ doit être rempli!")
+    @Size(min = 2, message = "Minimum de 2 caractere !")
     private String nom;
+    @NotBlank
+    @NotNull(message = "Ce champ doit être rempli!")
+    @Size(min = 3, message = "Minimum de 3 caractere !")
     private String prenom;
+    @Min(18)
+    @Max(100)
     private int age;
     private String email;
 }
